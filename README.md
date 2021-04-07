77b978d90ab0a4c107036bab0065af5ea4c0eb62
梁欢 <2275807201@qq.com>
2021-04-07 20:31:22 +0800

    可以读取ttyAMA0串口信息了

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Java串口编程例子
 项目详细介绍见博文：https://blog.csdn.net/wu_boy/article/details/112062071
 
 微信订阅号：八卦程序
+
+
+改造别人的代码，读取树莓派的信号。
+第一注意点：1.RXTXCommDriver源代码只会读取ttyS,ttySA,ttyUSB开头的设备。所以要对/dev/ttyAMA0建立软连接，如ln -s /dev/ttyAMA0 /dev/ttyS33
+但是软连接每次重启后会失效
+参考：https://blog.csdn.net/qq_42865331/article/details/90264722
+
+第二点：用pi用户启动java程序没有权限读取ttyAMA0串口信息。需要以root用户运行java程序。
--- src/main/java/cn/wu/demo/serialport/SerialPortTest.java
+++ src/main/java/cn/wu/demo/serialport/SerialPortTest.java
@@ -20,12 +20,15 @@ public class SerialPortTest {
 
     public static void main(String[] args) throws Exception{
 
+        log.info("开始调试");
+        System.in.read();
+
         // 显示端口标识符列表
         showPortNameList();
 
         System.out.println("program start1111");
 
-        String portName = "/dev/ttyS0";
+        String portName = "/dev/ttyS33";
         int baudRate = 9600;
 
         // 打开串口
@@ -39,22 +42,22 @@ public class SerialPortTest {
         });
 
 
-        log.info("开始发送数据11");
-        // 往串口发送数据
-        byte[] data = {1, 2, 3};
-        SerialPortUtils.write(serialPort, data);
-        log.info("发送数据结束22");
+//        log.info("开始发送数据11");
+//        // 往串口发送数据
+//        byte[] data = {1, 2, 3};
+//        SerialPortUtils.write(serialPort, data);
+//        log.info("发送数据结束22");
 
         /*// 关闭串口
         Thread.sleep(2000);
         SerialPortUtils.close(serialPort);*/
 
-        // 测试可用端口
-        List<String> strings = SerialPortUtils.listPortName();
-        log.info("列出所有可以打印的端口名：{}", strings);
-        strings.forEach(o -> System.out.println(o));
-
-        log.info("列出打印名结束333");
+//        // 测试可用端口
+//        List<String> strings = SerialPortUtils.listPortName();
+//        log.info("列出所有可以打印的端口名：{}", strings);
+//        strings.forEach(o -> System.out.println(o));
+//
+//        log.info("列出打印名结束333");
     }
 
     /**
